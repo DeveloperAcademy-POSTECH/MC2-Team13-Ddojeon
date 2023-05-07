1c6d3cf57ac724a4cf002bd94d227016a4f9d1ab
SeBin-Kwon <sbkwon98@gmail.com>
2023-05-07 21:30:58 +0900

    [Feat] Create MainView animation

---
+++ Chinggu/Chinggu/MainView.swift
@@ -0,0 +1,77 @@
+//
+//  MainView.swift
+//  Chinggu
+//
+//  Created by Sebin Kwon on 2023/05/06.
+//
+
+import SwiftUI
+import SpriteKit
+
+class GameScene: SKScene {
+    let stones = ["sample1", "sample2", "sample3"]
+    var currentStoneIndex = 0
+    var boxes: [SKSpriteNode] = []
+    
+    override func didMove(to view: SKView) {
+        physicsBody = SKPhysicsBody (edgeLoopFrom: frame)
+    }
+    
+    func addBox(at position: CGPoint) {
+        // 이미지가 랜덤으로 나오는 것
+        //        let index = Int.random(in: 0..<stones.count)
+        let texture = SKTexture(imageNamed: stones[currentStoneIndex])
+        let box = SKSpriteNode(texture: texture, size: CGSize(width: 100, height: 100))
+        let body = SKPhysicsBody(texture: texture, size: CGSize(width: 100, height: 100))
+        box.position = position
+        box.physicsBody = body
+        addChild(box)
+        boxes.append(box)
+        
+        // 이미지가 순서대로 나올 수 있도록 인덱스를 1씩 추가
+        currentStoneIndex += 1
+        if currentStoneIndex >= stones.count {
+            currentStoneIndex = 0
+        }
+        
+    }
+    
+    func resetBoxes() {
+        // 모든 박스 지우기
+        for box in boxes {
+            box.removeFromParent()
+        }
+        boxes.removeAll()
+        
+        // 인덱스 초기화
+        currentStoneIndex = 0
+    }
+}
+
+struct MainView: View {
+    @State private var showActionSheet = false
+    
+    let scene = GameScene(size: CGSize(width: 300, height: 400))
+    var body: some View {
+        VStack {
+            SpriteView(scene: scene)
+                .frame(width: 300, height: 400)
+            
+            // 칭찬돌 추가
+            Button(action: {
+                let position = CGPoint(x: scene.size.width/2,
+                                       y: scene.size.height - 50)
+                scene.addBox(at: position)
+            }, label: {
+                Text("Add Box")
+            })
+
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
