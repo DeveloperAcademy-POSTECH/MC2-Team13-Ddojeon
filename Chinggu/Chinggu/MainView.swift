c816be6217cf6eeb0bc3cb9db2818b414ebe692e
SeBin-Kwon <sbkwon98@gmail.com>
2023-05-10 20:17:45 +0900

    [Feat] Add expiration alert

--- Chinggu/Chinggu/MainView.swift
+++ Chinggu/Chinggu/MainView.swift
@@ -19,7 +19,6 @@ enum Weekday: String, CaseIterable {
 }
 
 class GameScene: SKScene {
-    let stones = ["sample1", "sample2", "sample3"]
     var boxes: [SKSpriteNode] = []
     
     override func didMove(to view: SKView) {
@@ -30,10 +29,10 @@ class GameScene: SKScene {
     
     func addBox(at position: CGPoint) {
         // 이미지가 랜덤으로 나오는 것
-        let index = Int.random(in: 0..<stones.count)
-        let texture = SKTexture(imageNamed: stones[index])
-        let box = SKSpriteNode(texture: texture, size: CGSize(width: 100, height: 100))
-        let body = SKPhysicsBody(texture: texture, size: CGSize(width: 100, height: 100))
+        let index = Int.random(in: 1..<99)
+        let texture = SKTexture(imageNamed: "stonery\(index)")
+        let box = SKSpriteNode(texture: texture)
+        let body = SKPhysicsBody(texture: texture, size: texture.size())
         box.position = position
         box.physicsBody = body
         addChild(box)
@@ -48,15 +47,15 @@ class GameScene: SKScene {
         boxes.removeAll()
     }
     
-//    func shake() {
-//            // Scene 좌우로 진동하는 애니메이션 추가
-//            let moveRight = SKAction.moveBy(x: 10, y: 0, duration: 0.05)
-//            let moveLeft = SKAction.moveBy(x: -20, y: 0, duration: 0.1)
-//            let moveCenter = SKAction.moveBy(x: 10, y: 0, duration: 0.05)
-//            let sequence = SKAction.sequence([moveRight, moveLeft, moveCenter])
-//            let repeatShake = SKAction.repeat(sequence, count: 3)
-//            run(repeatShake)
-//        }
+    //    func shake() {
+    //            // Scene 좌우로 진동하는 애니메이션 추가
+    //            let moveRight = SKAction.moveBy(x: 10, y: 0, duration: 0.05)
+    //            let moveLeft = SKAction.moveBy(x: -20, y: 0, duration: 0.1)
+    //            let moveCenter = SKAction.moveBy(x: 10, y: 0, duration: 0.05)
+    //            let sequence = SKAction.sequence([moveRight, moveLeft, moveCenter])
+    //            let repeatShake = SKAction.repeat(sequence, count: 3)
+    //            run(repeatShake)
+    //        }
 }
 
 struct MainView: View {
@@ -74,105 +73,105 @@ struct MainView: View {
     let scene = GameScene(size: CGSize(width: 350, height: 450))
     var body: some View {
         NavigationView {
-            VStack {
-                //MARK: 요일 변경하는 버튼
-                HStack {
-                    Text("매주")
-                        .tracking(-0.5)
-                    Button(action: {
-                        self.showActionSheet = true
-                    }, label: {
-                        Text(selectedWeekday?.rawValue ?? "뭔요일")
-                            .padding(.trailing, -7.0)
-                        Image(systemName: "arrowtriangle.down.square.fill")
-                    })
-                    .padding()
-                    .actionSheet(isPresented: $showActionSheet) {
-                        ActionSheet(title: Text("요일 변경"), message: nil, buttons: Weekday.allCases.map { weekday in
-                            if selectedWeekday == weekday {
-                                return nil
-                            } else {
-                                return .default(Text(weekday.rawValue)) {
-                                    self.showAlert = true
-                                    self.tempSeletedWeekday = weekday
+            ZStack {
+                Color.ddoPrimary.ignoresSafeArea()
+                VStack {
+                    //MARK: 요일 변경하는 버튼
+                    HStack {
+                        Text("매주")
+                            .tracking(-0.5)
+                        Button(action: {
+                            self.showActionSheet = true
+                        }, label: {
+                            Text(selectedWeekday?.rawValue ?? "뭔요일")
+                                .padding(.trailing, -7.0)
+                            Image(systemName: "arrowtriangle.down.square.fill")
+                        })
+                        .padding()
+                        .actionSheet(isPresented: $showActionSheet) {
+                            ActionSheet(title: Text("요일 변경"), message: nil, buttons: Weekday.allCases.map { weekday in
+                                if selectedWeekday == weekday {
+                                    return nil
+                                } else {
+                                    return .default(Text(weekday.rawValue)) {
+                                        self.showAlert = true
+                                        self.tempSeletedWeekday = weekday
+                                    }
                                 }
-                            }
-                        }.compactMap { $0 } + [.cancel()])
-                    }
-                    // 요일 변경할건지 얼럿
-                    .alert(isPresented: $showAlert) {
-                        Alert(title: Text("매주 \(tempSeletedWeekday?.rawValue ?? "뭔요일")"), message: Text("선택한 요일로 변경하시겠습니까?"), primaryButton: .default(Text("예")) {
-                            // OK 버튼을 눌렀을 때 선택한 요일 업데이트
-                            self.selectedWeekday = self.tempSeletedWeekday
-                            updateCanBreakBoxes()
-                            print(canBreakBoxes)
-                        }, secondaryButton: .cancel(Text("아니오")))
-                    }.padding(.horizontal, -19.0)
-                    Text("은 칭찬 저금통을 깨는 날!")
-                        .tracking(-0.5)
+                            }.compactMap { $0 } + [.cancel()])
+                        }
+                        // 요일 변경할건지 얼럿
+                        .alert(isPresented: $showAlert) {
+                            Alert(title: Text("매주 \(tempSeletedWeekday?.rawValue ?? "뭔요일")"), message: Text("선택한 요일로 변경하시겠습니까?"), primaryButton: .default(Text("예")) {
+                                // OK 버튼을 눌렀을 때 선택한 요일 업데이트
+                                self.selectedWeekday = self.tempSeletedWeekday
+                                updateCanBreakBoxes()
+                            }, secondaryButton: .cancel(Text("아니오")))
+                        }.padding(.horizontal, -19.0)
+                        Text("은 칭찬 저금통을 깨는 날!")
+                            .tracking(-0.5)
+                        Spacer()
+                        
+                        //MARK: 아카이브 페이지 링크
+                        NavigationLink(destination: ContentView()) {
+                            Image(systemName: "archivebox")
+                        }
+                    }.padding(.horizontal, 20.0)
                     Spacer()
                     
-                    //MARK: 아카이브 페이지 링크
-                    NavigationLink(destination: ContentView()) {
-                        Image(systemName: "archivebox")
-                    }
-                }.padding(.horizontal, 20.0)
-                Spacer()
-                
-                // 타이틀
-                Text("오늘은 어떤 칭찬을 해볼까요?✍️")
-                
-                //MARK: 칭찬 저금통
-                SpriteView(scene: scene)
-                    .frame(width: 350, height: 450)
-                    .background(.white)
-                    .cornerRadius(26)
-//                    .animation(canBreakBoxes ? shake : nil, value: canBreakBoxes)
-//                    .id(canBreakBoxes)
-                    .onTapGesture {
-                        if canBreakBoxes {
-                            scene.resetBoxes()
-                            showBreakAlert = false
-                        } else {
-                            showBreakAlert = true
-                        }
-                    }
-                // 중도 개봉 얼럿
-                    .alert(isPresented: $showBreakAlert) {
-                        Alert(title: Text("중도 개봉을 하시겠어요?"), primaryButton: .default(Text("예")) {
-                            // 저금통 초기화
-                            scene.resetBoxes()
-                        }, secondaryButton:.cancel(Text("아니오")))
-                    }
+                    // 타이틀
+                    Text("오늘은 어떤 칭찬을 해볼까요?✍️")
                     
-                Spacer()
-                // 칭찬돌 추가하는 버튼
-                Button(action: {
-//                    scene.shake()
-                    let position = CGPoint(x: scene.size.width/2,
-                                           y: scene.size.height - 50)
-                    scene.addBox(at: position)
-                }, label: {
-                    Text("칭찬하기")
-                })
-                .padding()
-                
-                // 저금통 깨는 버튼
-                //                Button(action: {
-                //                    scene.resetBoxes()
-                //                    canBreakBoxes = false
-                //                }, label: {
-                //                    Text("Break Box")
-                //                })
-                //                .disabled(!canBreakBoxes)
+                    //MARK: 칭찬 저금통
+                    SpriteView(scene: scene)
+                        .frame(width: 350, height: 450)
+                        .background(.white)
+                        .cornerRadius(26)
+                    //                    .animation(canBreakBoxes ? shake : nil, value: canBreakBoxes)
+                    //                    .id(canBreakBoxes)
+                        .onTapGesture {
+                            if scene.boxes.count > 0 {
+                                showBreakAlert = true
+                                    
+                            }
+                        }
+                    // 중도/만기일 개봉 얼럿
+                        .alert(isPresented: $showBreakAlert) {
+                            Alert(title: Text(canBreakBoxes ? "개봉 하시겠어요?" : "중도 개봉을 하시겠어요?"), primaryButton: .default(Text("예")) {
+                                // 저금통 초기화
+                                scene.resetBoxes()
+                            }, secondaryButton:.cancel(Text("아니오")))
+                        }
+                    
+                    Spacer()
+                    // 칭찬돌 추가하는 버튼
+                    Button(action: {
+                        //                    scene.shake()
+                        let position = CGPoint(x: scene.size.width/2,
+                                               y: scene.size.height - 50)
+                        scene.addBox(at: position)
+                    }, label: {
+                        Text("칭찬하기")
+                    })
+                    .padding()
+                    
+                    // 저금통 깨는 버튼
+                    //                Button(action: {
+                    //                    scene.resetBoxes()
+                    //                    canBreakBoxes = false
+                    //                }, label: {
+                    //                    Text("Break Box")
+                    //                })
+                    //                .disabled(!canBreakBoxes)
+                }
             }
         }
     }
     
-//    var shake: Animation {
-//        Animation.spring(response: 0.2, dampingFraction: 0.3, blendDuration: 0)
-//            .repeatForever(autoreverses: true)
-//    }
+    //    var shake: Animation {
+    //        Animation.spring(response: 0.2, dampingFraction: 0.3, blendDuration: 0)
+    //            .repeatForever(autoreverses: true)
+    //    }
     
     // 요일이 변경 될 때마다 현재 요일과 비교
     func updateCanBreakBoxes() {
