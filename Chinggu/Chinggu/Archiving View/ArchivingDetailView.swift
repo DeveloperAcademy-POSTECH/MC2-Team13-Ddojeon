ea725bc30df52e329f46a77ec169b3042bb79a02
JJunghyunY <50796114+JJunghyunY@users.noreply.github.com>
2023-05-13 02:58:07 +0900

    [Feat] Fix all ArchivingDetailView

--- Chinggu/Chinggu/Archiving View/ArchivingDetailView.swift
+++ Chinggu/Chinggu/Archiving View/ArchivingDetailView.swift
@@ -8,68 +8,91 @@
 import SwiftUI
 
 struct ArchivingDetailView: View {
-    
-    @State var cardText: String = "칭찬 글"
-    @State var cardData: String = "6"
-    @State var cardOrder: String = "45"
-    @State var cardDate: String = "2023. 05. 09"
-    
-    @State private var cardColor = Color.ddoOrange
-    
-    let colors = [
-        Color.ddoRed,
-        Color.ddoBlue,
-        Color.ddoPink,
-        Color.ddoGreen,
-        Color.ddoOrange
-    ]
-    
-    var body: some View {
-        // 하단 버튼 있으면 10, 아니면 30
-        VStack(spacing: 30){
-            
-            ArchivingCard(cardText: $cardText, cardData: $cardData, cardOrder: $cardOrder, cardDate: $cardDate, cardColor: $cardColor)
-            
-            HStack(spacing: 3){
-                Button(action: {
-                    self.cardColor = self.randomColor()
-                }, label: {
-                    Text("이전 칭찬")
-                        .frame(maxWidth: .infinity)
-                        .font(.title3)
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .padding(EdgeInsets(top: 18, leading: 20, bottom: 18, trailing: 20))
-                        .background(.black.opacity(0.7))
-                        .cornerRadius(15)
-                })
-                Spacer()
-                Button(action: {
-                    self.cardColor = self.randomColor()
-                }, label: {
-                    Text("다음 칭찬")
-                        .frame(maxWidth: .infinity)
-                        .font(.title3)
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .padding(EdgeInsets(top: 18, leading: 20, bottom: 18, trailing: 20))
-                        .background(.black.opacity(0.7))
-                        .cornerRadius(15)
-                })
-            }
-        }
-        .padding()
-        .background(Color(hex: 0xF9F9F3))
-    }
-    
-    private func randomColor() -> Color {
-        let randomIndex = Int.random(in: 0..<colors.count)
-        return colors[randomIndex]
-    }
-}
+	
+	@AppStorage("group") var groupOrder: Int = UserDefaults.standard.integer(forKey: "orderID")
+	@FetchRequest(
+		entity: ComplimentEntity.entity(),
+		sortDescriptors: []
+	) var allCompliments: FetchedResults<ComplimentEntity>
 
-struct ArchivingDetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        ArchivingDetailView()
-    }
+	@State var complimentOrder: Int16
+	@State var compliment: String = "칭찬 글"
+	@State var groupID: Int16 = 6
+	@State var order: Int16 = 45
+	@State var createDate: Date = Date()
+	
+	@State private var cardColor = Color.ddoOrange
+	
+	private let colors = [
+		Color.ddoRed,
+		Color.ddoBlue,
+		Color.ddoPink,
+		Color.ddoGreen,
+		Color.ddoOrange
+	]
+
+	var body: some View {
+		VStack(spacing: 30) {
+			ArchivingCard(compliment: $compliment,
+						  groupID: $groupID,
+						  order: $order,
+						  createDate: $createDate,
+						  cardColor: $cardColor)
+				.onAppear { loadCompliment() }
+			
+			HStack(spacing: 3) {
+				Button {
+					if complimentOrder > 1 {
+						complimentOrder -= 1
+						loadCompliment()
+					}
+				} label: {
+					Text("이전 칭찬")
+						.frame(maxWidth: .infinity)
+						.font(.title3)
+						.fontWeight(.bold)
+						.foregroundColor(.white)
+						.padding(EdgeInsets(top: 18, leading: 20, bottom: 18, trailing: 20))
+						.background(.black.opacity(0.7))
+						.cornerRadius(15)
+				}
+				.disabled(complimentOrder <= 1)
+				
+				Spacer()
+				
+				Button {
+					if complimentOrder < allCompliments.count {
+						complimentOrder += 1
+						loadCompliment()
+					}
+				} label: {
+					Text("다음 칭찬")
+						.frame(maxWidth: .infinity)
+						.font(.title3)
+						.fontWeight(.bold)
+						.foregroundColor(.white)
+						.padding(EdgeInsets(top: 18, leading: 20, bottom: 18, trailing: 20))
+						.background(.black.opacity(0.7))
+						.cornerRadius(15)
+				}
+				.disabled(complimentOrder >= allCompliments.count)
+			}
+		}
+		.padding()
+		.background(Color(hex: 0xF9F9F3))
+	}
+	
+	private func randomColor() -> Color {
+		let randomIndex = Int.random(in: 0..<colors.count)
+		return colors[randomIndex]
+	}
+	
+	private func loadCompliment() {
+		if let complimentEntity = PersistenceController.shared.loadCompliment(order: complimentOrder) {
+			compliment = complimentEntity.compliment ?? ""
+			groupID = complimentEntity.groupID
+			order = complimentEntity.order
+			createDate = complimentEntity.createDate ?? Date()
+		}
+	}
 }
