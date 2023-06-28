f81582a049af62a13632bd135be15ca941385ae3
chaekie <chaekie.kim@gmail.com>
2023-06-28 12:59:44 +0900

    아카이브 빈화면 디자인 추가

--- Chinggu/Chinggu/View/Archiving View/ArchivingView.swift
+++ Chinggu/Chinggu/View/Archiving View/ArchivingView.swift
@@ -19,13 +19,13 @@ struct ArchivingView: View {
     @State var isEditing = false
     
     var body: some View {
-        NavigationStack{
-            VStack(alignment: .leading){
-                if groupOrder >= 2 {
+        VStack {
+            if groupOrder >= 2 {
+                VStack(alignment: .leading) {
                     Text("\(groupOrder - 1)번의 상자를 열었고\n\(Compliment.count)번 칭찬했어요")
                         .font(.title3)
                         .fontWeight(.bold)
-                        .padding(.leading)
+                        .padding([.leading, .top])
                     List {
                         ForEach((1..<$groupOrder.wrappedValue).reversed(), id: \.self) { index in
                             Section(header: Text("\(index)번째 상자")) {
@@ -45,42 +45,63 @@ struct ArchivingView: View {
                                                         .lineLimit(1)
                                                 }
                                             })
-                                    } else { }
+                                    }
                                 }.onDelete(perform: delete)
                             }
                         }
                     }
-                    .environment(\.editMode, $editMode)
-                    .scrollContentBackground(.hidden)
-                    .navigationBarBackButtonHidden()
-                    .toolbar {
-                        ToolbarItem(placement: .navigationBarLeading) {
-                            Button(action: {
-                                dismiss()
-                            }) {
-                                Image(systemName: "chevron.backward")
-                                .foregroundColor(Color.black)
-                            }
-                        }
-                        ToolbarItem(placement: .navigationBarTrailing) {
-                            Button(action: {
-                                isEditing.toggle()
-                                editMode = isEditing ? .active : .inactive
-                            }) {
-                             Text(isEditing ? "완료" : "수정")
-                                    .foregroundColor(isEditing ? .blue : .black)
-                            }
-                        }
-                    }
-                }
-                else {
-                    Text("아직 열어본 칭찬상자가 없어요")
-                    //빈칭찬일 경우 넣을 이미지
                 }
             }
-            .accentColor(.red)
-            .padding(.top)
-            .background(Color.ddoPrimary)
+            else {
+                VStack {
+                    HStack {
+                        Text("아직 열어본\n칭찬상자가 없어요")
+                            .font(.title2)
+                            .lineSpacing(9)
+                            .bold()
+                        Spacer()
+                    }
+                    Spacer()
+                    VStack(spacing: 54) {
+                        Image("empty_archive")
+                        Text("첫 칭찬 상자를 열면\n기록을 확인할 수 있어요!")
+                            .multilineTextAlignment(.center)
+                            .font(.headline)
+                            .lineSpacing(3)
+                            .foregroundColor(Color(.systemGray2))
+                            .fontWeight(.regular)
+                            .fixedSize(horizontal: false, vertical: true)
+                    }
+                    Spacer()
+                }
+                .padding()
+            }
+            
+        }
+        .environment(\.editMode, $editMode)
+        .background(Color.ddoPrimary)
+        .scrollContentBackground(.hidden)
+        .navigationBarBackButtonHidden()
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button(action: {
+                    dismiss()
+                }) {
+                    Image(systemName: "chevron.backward")
+                        .foregroundColor(Color.black)
+                }
+            }
+            if groupOrder >= 2 {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        isEditing.toggle()
+                        editMode = isEditing ? .active : .inactive
+                    }) {
+                        Text(isEditing ? "완료" : "수정")
+                            .foregroundColor(isEditing ? .blue : .black)
+                    }
+                }
+            }
         }
     }
     
