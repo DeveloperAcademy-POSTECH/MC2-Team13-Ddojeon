fff5e0e7bafa1fd89f213c1744a90ecc2f93232e
JJunghyunY <wjdgus995@hanmail.net>
2024-02-20 04:21:18 +0900

    refact: dateManager tests done

--- Chinggu/Chinggu/Utilities/Date/DateManager.swift
+++ Chinggu/Chinggu/Utilities/Date/DateManager.swift
@@ -25,14 +25,19 @@ final class DateManager: DateCalculable {
     }
     
     //MARK: TimeInterval값 너무 복잡한것같아서 day값을 더한것으로 바꾸려 하는데 기존거 잘 돌아가고, 혹시모르니 테스트해보고 나중에 바꾸기
-    func nextWeekdayDateTest(from date: Date, weekdayString: String) -> Date {
-        let calendar = Calendar.current
-        guard let weekday = Weekday(rawValue: weekdayString)?.weekdayValue else { return date }
+    //테스트 완료했으나 AppStorage에서 Date값을 지원하지 않는다는걸 뒤늦게 발견..
+    func NEWnextWeekdayDate(_ weekdayString: String) -> Date {
+        //Timezone GMT0으로 설정하고, 오늘 자정을 기준으로 계산하기
+        var calendar = Calendar.current
+        calendar.timeZone = TimeZone(secondsFromGMT: 0)!
+        let todayJajung = calendar.startOfDay(for: Date())
+        guard let weekday = Weekday(rawValue: weekdayString)?.weekdayValue else { return Date() }
         
-        var nextDate = date
+        var nextDate = Date()
         for dayOffset in 1...7 {
-            nextDate = calendar.date(byAdding: .day, value: dayOffset, to: date)!
-            if calendar.component(.weekday, from: nextDate) == weekday {
+            if let date = calendar.date(byAdding: .day, value: dayOffset, to: todayJajung),
+               calendar.component(.weekday, from: date) == weekday {
+                nextDate = date
                 break
             }
         }
--- Chinggu/ChingguTests/DateManagerTests.swift
+++ Chinggu/ChingguTests/DateManagerTests.swift
@@ -20,11 +20,18 @@ final class DateManagerTests: XCTestCase {
     }
     
     func testNextWeekdayDate() throws {
-        let today = Calendar.current.startOfDay(for: Date())
-        let nextMonday = Calendar.current.nextDate(after: today, matching: DateComponents(weekday: 2), matchingPolicy: .nextTime)!
+        let calendar = Calendar.current
+        let nextWeekdayDate1 = Date(timeIntervalSince1970: sut.nextWeekdayDate("월요일"))
+        let nextWeekdayDate2 = sut.NEWnextWeekdayDate("월요일")
         
-        let nextMondayTimeInterval = sut.nextWeekdayDate("월요일")
+        let nextMonday1 = calendar.dateComponents([.weekday], from: nextWeekdayDate1)
+        let nextMonday2 = calendar.dateComponents([.weekday], from: nextWeekdayDate2)
         
-        XCTAssertEqual(nextMonday.timeIntervalSince1970, nextMondayTimeInterval, accuracy: 1.0)
+        print(nextWeekdayDate1, nextWeekdayDate2)
+        //before: 2024-02-25 15:00:00 +0000 2024-02-25 18:46:01 +0000
+        //after: 2024-02-25 15:00:00 +0000 2024-02-26 00:00:00 +0000
+        //한국은 GMT +9 이므로 9시간 더하면 값이 동일함, 테스트 통과
+
+        XCTAssertEqual(nextMonday1, nextMonday2)
     }
 }
